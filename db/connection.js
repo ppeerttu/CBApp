782c4dd506bcd7363149278b4d285a2ad27f501f
Perttu Kärnä <Perttu@Perttu-MacBook-Air.local>
2016-12-25 17:27:07 +0200

    Set database from SQLite to postgres

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('cbapp', '', '', {
-  dialect: 'sqlite',
-  storage: 'db/CBApp.database'
+var sequelize = new Sequelize('postgres://abajeoneynnezq:77960af7b420a65441cfdb75a2cdeb22eaf7d686f7be725326b7a1b980b08ef9@ec2-54-243-38-139.compute-1.amazonaws.com:5432/d7m432pddc2780', {
+  dialect: 'postgres',
+  protocol: 'postgres'
 });
 
 module.exports = {
