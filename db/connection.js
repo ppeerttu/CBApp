787400be2ba588a196d554d9dffc2d58be780d2d
Perttu Kärnä <Perttu@Perttu-MacBook-Air.local>
2017-01-02 13:29:47 +0200

    updated to postgres database

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,17 @@
 var Sequelize = require('sequelize');
-/* FOR POSTGRESQL DATABASE
+/* FOR POSTGRESQL DATABASE */
 var sequelize = new Sequelize('postgres-database-url', {
   dialect: 'postgres',
   protocol: 'postgres'
-});*/
+});
 
-/* FOR LOCAL DEVELOPMENT*/
+/* FOR LOCAL DEVELOPMENT
 var sequelize = new Sequelize('cbapp', '', '', {
   dialect: 'sqlite',
   storage: 'db/CBApp.database'
-});
+});*/
 
 module.exports = {
   DataTypes: Sequelize,
   sequelize: sequelize
 };
-
-
