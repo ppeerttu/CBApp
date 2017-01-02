ce874ef095d40ddd6e8152e857ed3860744730e3
Perttu Kärnä <Perttu@Perttu-MacBook-Air.local>
2017-01-02 14:35:50 +0200

    New database

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 /* FOR POSTGRESQL DATABASE */
-var sequelize = new Sequelize('postgres://abajeoneynnezq:77960af7b420a65441cfdb75a2cdeb22eaf7d686f7be725326b7a1b980b08ef9@ec2-54-243-38-139.compute-1.amazonaws.com:5432/d7m432pddc2780', {
+var sequelize = new Sequelize('postgres://wzdpolrthisfoi:00375316fb321210f03603987e96d84dcb8d4d9fe72d601f5f61d517ce68af3f@ec2-54-235-173-161.compute-1.amazonaws.com:5432/d3ci3beb4kh7it', {
   dialect: 'postgres',
   protocol: 'postgres'
 });
