eb3c0e1dd45b270db2b64331d15df683435aea6b
Perttu Kärnä <Perttu@Perttu-MacBook-Air.local>
2017-01-02 15:12:41 +0200

    Updated app.min.js

--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -14,11 +14,9 @@ CBApp.controller('UserController',['$rootScope', '$scope', 'APIService', '$locat
     $scope.onFBlogin = function () {
         FB.login(function (response) {
             if (response.authResponse) {
-                console.log()
                 FB.api('/me', {
                     fields: ['first_name', 'last_name', 'email']
                 }, function (response) {
-                    console.log(response);
                     var newUser = {
                         firstName: response.first_name,
                         lastName: response.last_name,
@@ -41,9 +39,9 @@ CBApp.controller('UserController',['$rootScope', '$scope', 'APIService', '$locat
     $scope.register = function () {
         if (typeof $scope.newUser !== 'undefined') {
             if ($scope.newUser.firstName.length < 2 || $scope.newUser.nickname.length < 3 || $scope.newUser.password.length < 8) {
-                console.log('User info not valid! Registration denied.');
+                $scope.regError = 'User info not valid! Registration denied.';
             } else {
-                if ($scope.newUser.lastName === 'undefined' || $scope.newUser.lastName === '' || $scope.newUser.lastName === null) {
+                if (typeof $scope.newUser.lastName === 'undefined' || $scope.newUser.lastName === '' || $scope.newUser.lastName === null) {
                     $scope.newUser.lastName = 'not_set';
                 }
                 $scope.newUser.fbId = 'not_set';
@@ -61,7 +59,6 @@ CBApp.controller('UserController',['$rootScope', '$scope', 'APIService', '$locat
     $scope.login = function () {
         if (typeof $scope.user !== 'undefined') {
             APIService.login($scope.user).success(function (user) {
-                console.log(user);
                 $rootScope.profile = user;
                 $rootScope.profile.RoomId = null;
                 $cookies.putObject('profile', $rootScope.profile);
